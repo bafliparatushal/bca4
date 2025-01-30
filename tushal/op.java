d62bebab5fefc92641e94470e05299b907271de8
bafliparatushal <bafliparatushal23@gmail.com>
2025-01-30 08:52:56 +0530

    Add files via upload

---
+++ tushal/op.java
@@ -0,0 +1,29 @@
+class op
+{
+	public static void main(String [] args)
+	{
+		int decimal= 50;
+		int binary=0101010001;
+		int octal=0143;
+		int hexa=0x12;
+		
+		float value=0.124F;
+		
+		char c='A';
+		
+		String str="tushal";
+		
+		
+		boolean t= true;
+		boolean f= false;
+				
+				
+		System.out.println("decimal:"+decimal);
+		System.out.println("binary:"+binary);
+		System.out.println("octal:"+octal);	
+		System.out.println("hexa:"+hexa);
+		System.out.println("float:"+value);
+		System.out.println("chracter:"+c);
+		System.out.println("String:"+str);	
+	}
+}
